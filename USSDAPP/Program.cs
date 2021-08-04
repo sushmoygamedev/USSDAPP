4b8e2dd11c5ab7ac57b354f025989ba90426dda1
sushmoygamedev <sushmoygamedev@gmail.com>
2021-08-05 00:22:14 +0600

    Building Structure

--- USSDAPP/Program.cs
+++ USSDAPP/Program.cs
@@ -6,7 +6,13 @@ namespace USSDAPP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("DigiCash\n");
+            string[] OptionList = new string[] { "Send Money", "Send Money to Non-bKash User", "Mobile Recharge", "Payment", "Cash Out", "Pay Bill", "Get Tk 75 bonus now", "My bKash", "Reset PIN" };
+
+            for (int i = 0; i < OptionList.Length; i++)
+            {
+                Console.WriteLine(i + ". " + OptionList[i]);
+            }
         }
     }
 }
